2902df1df78ad320532f59a2d247276bbe9fab50
JiangXJ1 <jiangxiaojunok@qq.com>
2019-09-20 14:04:54 +0800

    提交PostProcessing

---
+++ packge_libary/com.unity.postprocessing@2.1.4/PostProcessing/Runtime/Monitors/LightMeterMonitor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UnityEngine.Rendering.PostProcessing
+{
+    /// <summary>
+    /// This class holds settings for the Light Meter monitor.
+    /// </summary>
+    [Serializable]
+    public sealed class LightMeterMonitor : Monitor
+    {
+        /// <summary>
+        /// The width of the rendered light meter.
+        /// </summary>
+        public int width = 512;
+
+        /// <summary>
+        /// The height of the rendered light meter.
+        /// </summary>
+        public int height = 256;
+
+        /// <summary>
+        /// Should we display grading and tonemapping curves on top?
+        /// </summary>
+        /// <remarks>
+        /// This only works when <see cref="GradingMode.HighDefinitionRange"/> is active.
+        /// </remarks>
+        public bool showCurves = true;
+
+        internal override bool ShaderResourcesAvailable(PostProcessRenderContext context)
+        {
+            return context.resources.shaders.lightMeter && context.resources.shaders.lightMeter.isSupported;
+        }
+
+        internal override void Render(PostProcessRenderContext context)
+        {
+            CheckOutput(width, height);
+
+            var histogram = context.logHistogram;
+
+            var sheet = context.propertySheets.Get(context.resources.shaders.lightMeter);
+            sheet.ClearKeywords();
+            sheet.properties.SetBuffer(ShaderIDs.HistogramBuffer, histogram.data);
+
+            var scaleOffsetRes = histogram.GetHistogramScaleOffsetRes(context);
+            scaleOffsetRes.z = 1f / width;
+            scaleOffsetRes.w = 1f / height;
+
+            sheet.properties.SetVector(ShaderIDs.ScaleOffsetRes, scaleOffsetRes);
+
+            if (context.logLut != null && showCurves)
+            {
+                sheet.EnableKeyword("COLOR_GRADING_HDR");
+                sheet.properties.SetTexture(ShaderIDs.Lut3D, context.logLut);
+            }
+
+            var cmd = context.command;
+            cmd.BeginSample("LightMeter");
+            cmd.BlitFullscreenTriangle(BuiltinRenderTextureType.None, output, sheet, 0);
+            cmd.EndSample("LightMeter");
+        }
+    }
+}
